c5c59e758b1de95459a49481ceaa46d38319fa90
Tuhin Sharma <tuhinsharma@Tuhins-MacBook-Pro.local>
2020-11-19 14:07:23 +0530

    refactored and unittest framework set

--- tests/test_pybandit.py
+++ tests/test_pybandit.py
@@ -4,5 +4,9 @@ from pybandit import __version__
 
 
 class TestPybandit(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestPybandit, self).__init__(*args, **kwargs)
+
     def test_version(self):
-        assert __version__ == '0.0.1'
+        self.assertEqual(__version__, '0.0.1')
